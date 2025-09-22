05ec7452e04587cfdc702e34d83ea8b18c29a505
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-09-22 12:36:09 +0300

    complete lab

---
+++ models.py
@@ -0,0 +1,53 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    # Relationship to Review
+    reviews = relationship("Review", back_populates="customer")
+    
+    # Association proxy: get items through reviews
+    items = association_proxy("reviews", "item")
+
+    # Serialization rules to avoid recursion
+    serialize_rules = ("-reviews.customer",)
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    # Relationship to Review
+    reviews = relationship("Review", back_populates="item")
+
+    # Serialization rules to avoid recursion
+    serialize_rules = ("-reviews.item",)
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+
+    # Relationships
+    customer = relationship("Customer", back_populates="reviews")
+    item = relationship("Item", back_populates="reviews")
+
+    # Serialization rules to avoid recursion
+    serialize_rules = ("-customer.reviews", "-item.reviews",)
+
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.name}, {self.price}>'
